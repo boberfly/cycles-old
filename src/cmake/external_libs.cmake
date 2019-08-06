a227d21da00eb4ca92f14c9b45748497d9fcbd12
Alex Fuller <boberfly@gmail.com>
2019-08-05 20:26:50 -0700

    CMake build fixes that are Gaffer-specific
    (cherry picked from commit d1073dedba74b09aabe31cd55aaaad0d3478c1df)

--- src/cmake/external_libs.cmake
+++ src/cmake/external_libs.cmake
@@ -145,6 +145,13 @@ if(CYCLES_STANDALONE_REPOSITORY)
     find_package(Gflags REQUIRED)
   endif()
 
+  ####
+  # OpenSubdiv
+  if(WITH_CYCLES_OPENSUBDIV)
+    find_package(OpenSubdiv REQUIRED)
+    set(WITH_OPENSUBDIV ON)
+  endif()
+
   unset(_lib_DIR)
 else()
   set(LLVM_LIBRARIES ${LLVM_LIBRARY})
