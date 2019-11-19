62e9e09333b26186ed05eccc42f86439f2183f09
Alex Fuller <boberfly@gmail.com>
2019-11-18 23:46:06 -0800

    Squash merge of cycles_texture_cache branch

--- src/kernel/closure/bsdf.h
+++ src/kernel/closure/bsdf.h
@@ -194,7 +194,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals *kg,
                                      omega_in,
                                      &domega_in->dx,
                                      &domega_in->dy,
-                                     pdf);
+                                     pdf,
+                                     sd);
       break;
     case CLOSURE_BSDF_REFRACTION_ID:
       label = bsdf_refraction_sample(sc,
@@ -208,7 +209,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals *kg,
                                      omega_in,
                                      &domega_in->dx,
                                      &domega_in->dy,
-                                     pdf);
+                                     pdf,
+                                     sd);
       break;
     case CLOSURE_BSDF_TRANSPARENT_ID:
       label = bsdf_transparent_sample(sc,
@@ -242,7 +244,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals *kg,
                                          omega_in,
                                          &domega_in->dx,
                                          &domega_in->dy,
-                                         pdf);
+                                         pdf,
+                                         sd);
       break;
     case CLOSURE_BSDF_MICROFACET_MULTI_GGX_ID:
     case CLOSURE_BSDF_MICROFACET_MULTI_GGX_FRESNEL_ID:
@@ -276,7 +279,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals *kg,
                                                      &domega_in->dx,
                                                      &domega_in->dy,
                                                      pdf,
-                                                     &sd->lcg_state);
+                                                     &sd->lcg_state,
+                                                     sd);
       break;
     case CLOSURE_BSDF_MICROFACET_BECKMANN_ID:
     case CLOSURE_BSDF_MICROFACET_BECKMANN_ANISO_ID:
@@ -293,7 +297,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals *kg,
                                               omega_in,
                                               &domega_in->dx,
                                               &domega_in->dy,
-                                              pdf);
+                                              pdf,
+                                              sd);
       break;
     case CLOSURE_BSDF_ASHIKHMIN_SHIRLEY_ID:
     case CLOSURE_BSDF_ASHIKHMIN_SHIRLEY_ANISO_ID:
