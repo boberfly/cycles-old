e65789e66f414e2da3ee1c9b95938ada7098ce3d
Alex <boberfly@gmail.com>
2021-10-10 22:43:25 -0700

    Gaffer uses an older TBB, we need TBB_PREVIEW_GLOBAL_CONTROL 1 set

--- src/util/util_tbb.h
+++ src/util/util_tbb.h
@@ -27,6 +27,8 @@
 #include <tbb/task_arena.h>
 #include <tbb/task_group.h>
 
+#define TBB_PREVIEW_GLOBAL_CONTROL 1
+
 #if TBB_INTERFACE_VERSION_MAJOR >= 10
 #  define WITH_TBB_GLOBAL_CONTROL
 #  include <tbb/global_control.h>
